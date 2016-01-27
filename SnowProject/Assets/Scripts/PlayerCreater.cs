0ee72d03cc7cde181d34326a0dffc947bfa0225d
masashi37 <mssksk837@gmail.com>
2016-01-27 12:38:12 +0900

    StartUp
    引力は追加済み

---
+++ SnowProject/Assets/Scripts/PlayerCreater.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCreater : MonoBehaviour
+{
+    int time = 0;
+
+    [SerializeField]
+    GameObject playerPrefab = null;
+
+    //-------------------------------------------
+
+    void Update()
+    {
+        time++;
+
+        if (time % (60 * 2) == 0)
+        {
+            CreatePlayer();
+        }
+    }
+
+    void CreatePlayer()
+    {
+        var obj = Instantiate(playerPrefab);
+
+        obj.transform.localPosition =
+            new Vector3(
+                Random.Range(-5, 5),
+                Random.Range(-5, 5),
+                Random.Range(-5, 5));
+    }
+}
